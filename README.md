744bb313aaaf315d9e619815b14533e75422f938
rrkai <59231078+rrkai@users.noreply.github.com>
2022-04-18 23:23:24 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# OpenWrt X86-64 自用版云编译项目
+
+## 固件来源：
+
+P3TERX云编译脚本地址：[https://github.com/P3TERX/Actions-OpenWrt](https://github.com/P3TERX/Actions-OpenWrt)
+
+lean固件源码地址：[https://github.com/coolsnowwolf/lede](https://github.com/coolsnowwolf/lede)
+
+云编译脚本借鉴：[https://github.com/firkerword/openwrt-Exclusive](https://github.com/firkerword/openwrt-Exclusive)
+
+由衷感谢所有为openwrt无私奉献的大佬们。
+
+## 固件说明：
+
+Lean源码版本，内核为***5.10***版
+
+自动每15天更新一次，手动不定时更新
+
+> `ip：192.168.1.1 密码：password`
+
+## 固件版本：
+
+### Suse版本为自用版本
+
+版本特点:插件只保留几种必需.可做为主路由长期稳定使用
+
+### Plus版本为加强版本
+
+版本特点:
+
+### 具体插件列表可见下方的固件截图
+
+## 固件下载
+### 点击[Releases](https://github.com/rrkai/OpenWrt-Liao/releases) 选择需要的版本
+![avatar](https://raw.githubusercontent.com/rrkai/OpenWrt-Liao/main/PNG/down.png)
+
+## 固件截图：
+### Suse 自用版
+![avatar](https://raw.githubusercontent.com/rrkai/OpenWrt-Liao/main/PNG/suse.jpg)
+
+### Plus 加强版
+![avatar](https://raw.githubusercontent.com/rrkai/OpenWrt-Liao/main/PNG/plus.png)
---
+++ diy5.10suse.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+# sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
+# mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+# git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
+# git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+git clone https://github.com/TorBoxCode/luci-app-eqos.git package/luci-app-eqos
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
+# git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
+# git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
