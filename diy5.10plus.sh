6903e56a259e1afaedd46b2b9ef8cfc4467eaf54
rrkai <59231078+rrkai@users.noreply.github.com>
2022-04-23 23:24:44 +0800

    Update diy5.10plus.sh

--- diy5.10plus.sh
+++ diy5.10plus.sh
@@ -24,7 +24,6 @@ svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliy
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
