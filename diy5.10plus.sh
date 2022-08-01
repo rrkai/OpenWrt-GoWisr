01fc7ec2f3d4c7c96309d4056213fc835c7443c2
rrkai <59231078+rrkai@users.noreply.github.com>
2022-08-01 18:55:13 +0800

    Update diy5.10plus.sh

--- diy5.10plus.sh
+++ diy5.10plus.sh
@@ -10,6 +10,8 @@
 
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.18/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.19/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
