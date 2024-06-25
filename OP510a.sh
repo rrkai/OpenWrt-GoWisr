a6f4990b26c78e37ce498606cb271c869d531842
rrkai <59231078+rrkai@users.noreply.github.com>
2024-06-25 14:27:58 +0800

    Update OP510a.sh

--- OP510a.sh
+++ OP510a.sh
@@ -8,8 +8,9 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i "s/KERNEL_PATCHVER:=*.*/KERNEL_PATCHVER:=5.10/g" lede/target/linux/x86/Makefile
-sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.10/g" lede/target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
