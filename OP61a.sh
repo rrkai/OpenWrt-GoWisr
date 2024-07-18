059b5c477ee2e1fb0db3caca35fdae9381818ccf
rrkai <59231078+rrkai@users.noreply.github.com>
2024-07-18 12:00:54 +0800

    Update OP61a.sh

--- OP61a.sh
+++ OP61a.sh
@@ -41,7 +41,7 @@ git clone https://github.com/rrkai/luci-app-eqosplus package/luci-app-eqosplus
 # git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 # git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 # git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
