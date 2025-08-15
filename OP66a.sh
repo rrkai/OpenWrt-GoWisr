a41447348966bbd9f877862238aa6c5f2a9de829
rrkai <59231078+rrkai@users.noreply.github.com>
2025-08-15 13:18:27 +0800

    Update OP66a.sh

--- OP66a.sh
+++ OP66a.sh
@@ -13,8 +13,7 @@ sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makef
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 sed -i '/openwrt-24.10/d' feeds.conf.default
-sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
-# sed -i '2i src-git luci https://github.com/coolsnowwolf/luci.git' feeds.conf.default
+sed -i '2i src-git luci https://github.com/coolsnowwolf/luci.git' feeds.conf.default
 
 
 function merge_package(){
