5650a4e7ece9cac1e7ead1a55cb0cfcb207d1268
rrkai <59231078+rrkai@users.noreply.github.com>
2025-11-18 12:29:02 +0800

    Update OP66a.sh

--- OP66a.sh
+++ OP66a.sh
@@ -3,9 +3,7 @@ sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefi
 sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 sed -i '/openwrt-24.10/d' feeds.conf.default
 
-# 只取消注释非 luci 相关的行，或者先删除重复的 luci 源再取消注释
-# 避免重复 luci 源：先删除所有 luci 相关行（如果有的话），然后取消注释
-sed -i '/luci/d' feeds.conf.default
+# 移除这行：sed -i '/luci/d' feeds.conf.default
 
 function merge_package(){
     repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
@@ -24,25 +22,25 @@ rm -rf package/custom; mkdir package/custom
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git    package/luci-theme-argon-18.06
-git clone https://github.com/jerrykuku/lua-maxminddb.git   package/lua-maxminddb
-git clone https://github.com/rrkai/luci-app-vssr.git   package/luci-app-vssr
-git clone https://github.com/kenzok78/luci-app-fileassistant.git   package/luci-app-fileassistant
-git clone https://github.com/zzsj0928/luci-app-pushbot   package/luci-app-pushbot
-git clone https://github.com/rrkai/luci-app-eqosplus   package/luci-app-eqosplus
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git   package/openwrt-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git   package/passwall
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# git clone --depth=1 https://github.com/fw876/helloworld.git   package/helloworld
-# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt   package/aliyundrive-webdav
-# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui   package/app-store-ui
-# git clone https://github.com/thinktip/luci-theme-neobird.git   package/luci-theme-neobird
-# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git   package/luci-theme-edge
-# git clone https://github.com/sirpdboy/luci-theme-opentopd.git   package/luci-theme-opentopd
-# git clone https://github.com/tty228/luci-app-serverchan.git   package/luci-app-serverchan
-# git clone https://github.com/project-lede/luci-app-godproxy.git   package/luci-app-godproxy
-# git clone https://github.com/garypang13/luci-app-dnsfilter.git   package/luci-app-dnsfilter
-# git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git   package/luci-app-UUGameAcc
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git      package/luci-theme-argon-18.06
+git clone https://github.com/jerrykuku/lua-maxminddb.git     package/lua-maxminddb
+git clone https://github.com/rrkai/luci-app-vssr.git     package/luci-app-vssr
+git clone https://github.com/kenzok78/luci-app-fileassistant.git     package/luci-app-fileassistant
+git clone https://github.com/zzsj0928/luci-app-pushbot     package/luci-app-pushbot
+git clone https://github.com/rrkai/luci-app-eqosplus     package/luci-app-eqosplus
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git     package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git     package/passwall
+git clone https://github.com/vernesong/OpenClash.git   package/luci-app-openclash
+git clone https://github.com/sbwml/v2ray-geodata   package/v2ray-geodata
+# git clone --depth=1 https://github.com/fw876/helloworld.git     package/helloworld
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt     package/aliyundrive-webdav
+# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui     package/app-store-ui
+# git clone https://github.com/thinktip/luci-theme-neobird.git     package/luci-theme-neobird
+# git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git     package/luci-theme-edge
+# git clone https://github.com/sirpdboy/luci-theme-opentopd.git     package/luci-theme-opentopd
+# git clone https://github.com/tty228/luci-app-serverchan.git     package/luci-app-serverchan
+# git clone https://github.com/project-lede/luci-app-godproxy.git     package/luci-app-godproxy
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git     package/luci-app-dnsfilter
+# git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git     package/luci-app-UUGameAcc
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
